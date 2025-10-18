b27db7771060b44be35249c6bc274542e07f3f36
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2025-10-18 11:17:16 +0100

    feat: add random ships function for 1st ship

--- src/game-controller.js
+++ src/game-controller.js
@@ -2,9 +2,11 @@ import { displayBothBoards } from "./dom-view";
 import { player } from "./game-logic-tested/player";
 
 export const gameController = (function () {
-  const players = createPlayers();
-
-  prePopulateBothShips(players);
+  const user = player();
+  const computer = player();
+  const players = [user, computer];
+  // const ships = createShips();
+  // prePopulateBothShips(players);
 
   displayBothBoards(players);
 
@@ -16,22 +18,79 @@ export const gameController = (function () {
       players[0].receiveAttack(row2, column2);
       displayBothBoards(players);
     },
+    placeUserShipsRandom: function () {
+      const userShips = getRandomShips();
+      userShips.forEach((element) => {
+        let shipRow = Number(element.charAt(0));
+        let shipColumn = Number(element.charAt(2));
+        user.placeShip(shipRow, shipColumn);
+      });
+      displayBothBoards(players);
+    },
   };
 
   // create conditions to end game after all ships sunk here
 
   // implement system to allow players to place their ships via coord or button to random
 
-  // extra credit:
-  // implement drag and drop to place ships ?
-  // create 2-player option ?
   // polish intelligence of computer by having it try adjacent slots after hit
 })();
 
-function createPlayers() {
-  const user = player();
-  const computer = player();
-  return [user, computer];
+function getRandomShips() {
+  const ships = [];
+  const ship1 = getShipLength4Row();
+  ships.push(...ship1);
+
+  // const ship2 = getShipLength3Row();
+
+  console.log(ships);
+  return ships;
+}
+
+function getShipLength4Row() {
+  let shipStart = [getRandomInt(10), getRandomInt(10)];
+  let shipMiddle1;
+  let shipMiddle2;
+  let shipEnd;
+
+  if (shipStart[1] > 5) {
+    shipMiddle1 = [shipStart[0], shipStart[1] - 1];
+    shipMiddle2 = [shipStart[0], shipStart[1] - 2];
+    shipEnd = [shipStart[0], shipStart[1] - 3];
+  } else {
+    shipMiddle1 = [shipStart[0], shipStart[1] + 1];
+    shipMiddle2 = [shipStart[0], shipStart[1] + 2];
+    shipEnd = [shipStart[0], shipStart[1] + 3];
+  }
+  return [
+    shipStart.toString(),
+    shipMiddle1.toString(),
+    shipMiddle2.toString(),
+    shipEnd.toString(),
+  ];
+}
+
+function getShipLength3Row() {
+  let shipStart = [getRandomInt(10), getRandomInt(10)];
+  let shipMiddle;
+  let shipEnd;
+
+  if (shipStart[1] > 5) {
+    shipMiddle = [shipStart[0], shipStart[1] - 1];
+    shipEnd = [shipStart[0], shipStart[1] - 3];
+  } else {
+    shipMiddle = [shipStart[0], shipStart[1] + 1];
+    shipEnd = [shipStart[0], shipStart[1] + 3];
+  }
+  return [
+    shipStart.toString(),
+    shipMiddle.toString(),
+    shipEnd.toString(),
+  ];
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 function prePopulateBothShips(players) {
@@ -60,8 +119,4 @@ function getComputerAttack(computer) {
   } while (boardValue > 1);
 
   return [row, column];
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
 }
--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function gameSetup() {
 
 randomButton.addEventListener("click", (event) => {
   event.preventDefault();
-  // placeShipsrandom();
+  gameController.placeUserShipsRandom();
   startGameButton.style.display = "inline-block";
 });
 
